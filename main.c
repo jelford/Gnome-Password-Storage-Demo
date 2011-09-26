a5b6af2c9751f46fde846b73ffbd655926192eb6
James Elford <james.elford08@imperial.ac.uk>
2011-09-26 17:02:36 +0100

    Also retrieve the password from memory

--- main.c
+++ main.c
@@ -1,23 +1,41 @@
 #include <gnome-keyring.h>
+#include <gnome-keyring-memory.h>
+
 #include <glib.h>
 #include <stdio.h>
 
 int main (int argc, char** argv) {
     GnomeKeyringResult result;
+    gchar* password = gnome_keyring_memory_alloc(8192);
     result = gnome_keyring_store_password_sync(
             GNOME_KEYRING_NETWORK_PASSWORD, // Password type
             NULL,                           // Which keyring
             "My test password",             // Description
             "helloWorld",                   // The password
-            "user", "james",
+            "user", "some_user",
             "server", "localhost",
             NULL);
     if (result == GNOME_KEYRING_RESULT_OK) {
-        g_print("Successfully stored password to default keyring");
-        return 0;
-    }    
-    else {
-        g_print("Failed to store password");
+        g_print("Successfully stored password to default keyring\n");
+        result = gnome_keyring_find_password_sync(
+                GNOME_KEYRING_NETWORK_PASSWORD,
+                &password,
+                "user", "james",
+                "server", "localhost",
+                NULL);
+        if (result == GNOME_KEYRING_RESULT_OK) {
+            g_print("Successfully retrived password from memory\n");
+            g_print("Password: %s\n", password);
+            gnome_keyring_free_password(password);
+            return 0;
+        } else {
+            g_print("Could not get password from memory\n");
+            return 1;
+        }
+    } else {
+        g_print("Failed to store password\n");
         return 1;
     }
+
+
 }
